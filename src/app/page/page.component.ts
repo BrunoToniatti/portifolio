68868fc735bb4a372273e732b52c8311d51e318b
Bruno Toniatti <toniattibruno08@gmail.com>
2025-06-27 18:16:56 -0300

    Meus projetos

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IntroComponent } from './intro/intro.component';
+import { ProjectsComponent } from "./projects/projects.component";
 
 
 @Component({
   selector: 'app-page',
   standalone: true,
-  imports: [IntroComponent],
+  imports: [IntroComponent, ProjectsComponent],
   templateUrl: './page.component.html',
   styleUrl: './page.component.scss'
 })
---
+++ src/app/page/projects/projects.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+
+  projects = [
+    {
+      img: "../../../assets/projects/lais-principal.png",
+      title: "Portifólio pessoal",
+      subtitle: "Desenvolvi um portifólio pessoal para aumentar a presença online de minha cliente.",
+    },
+    {
+      img: "../../../assets/projects/lais-calendar.png",
+      title: "Calendário agendamento inteligente",
+      subtitle: "Desenvolvi um agendamento, com gerador de horário automático de acordo com os agendamentos no dia e o tempo de cada procedimento agendado.",
+    },
+    {
+      img: "../../../assets/projects/lais-pwa.png",
+      title: "Aplicativo gerenciador",
+      subtitle: "Um aplicativo para gerenciar seus atendimentos e seus serviços, conectado diretamente com o seu portifólio.",
+    },
+  ]
+
+}
